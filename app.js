34f0ff67934b747cdd42a25c391159444dbb9dda
15239136251 <2532281393@qq.com>
2023-04-10 14:08:51 +0800

    添加监听事件

---
+++ app.js
@@ -0,0 +1,22 @@
+/* app.js */
+
+module.exports = app => {
+    app.once('server', (server) => {
+        // websocket
+        console.log("🚀 ~ file: app.js:7 ~ app.once ~ server:", server)
+    });
+    app.on('error', (err, ctx) => {
+        // report error
+        console.log("🚀 ~ file: app.js:10 ~ app.on ~ error:", err)
+    });
+    app.on('request', (ctx) => {
+        console.log("🚀 ~ file: app.js:13 ~ app.on ~ request:", ctx)
+        // log receive request
+    });
+    app.on('response', (ctx) => {
+        // ctx.starttime is set by framework
+        const used = Date.now() - ctx.starttime;
+        console.log("🚀 ~ file: app.js:21 ~ app.on ~ response:", ctx)
+        // log total cost
+    });
+};
