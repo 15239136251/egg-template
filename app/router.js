7674614d8c8af07d32422723167efae4232073f5
15239136251 <2532281393@qq.com>
2023-04-10 15:27:01 +0800

    路由拆分

--- app/router.js
+++ app/router.js
@@ -4,10 +4,6 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller, jwt } = app;
-  router.get('/', controller.home.index);
-  router.get('/userlist', controller.home.userlist);
-
-  router.post('/user/login', controller.user.login);
-  router.post('/user', jwt, controller.user.index);
+  require('./router/home')(app);
+  require('./router/user')(app);
 };
---
+++ app/router/user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = (app) => {
+  const { router, controller, jwt } = app;
+  
+  router.post('/user/login', controller.user.login);
+  router.post('/user', jwt, controller.user.index);
+};
