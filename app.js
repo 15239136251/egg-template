cd4aad48c345c675936a16c3c73599215ca526de
15239136251 <2532281393@qq.com>
2023-04-10 15:19:32 +0800

    打印信息修改

--- app.js
+++ app.js
@@ -3,20 +3,19 @@
 module.exports = app => {
     app.once('server', (server) => {
         // websocket
-        console.log("🚀 ~ file: app.js:7 ~ app.once ~ server:", server)
+        console.log("🚀 ~ file: app.js:7 ~ app.once ~ server:")
     });
     app.on('error', (err, ctx) => {
         // report error
         console.log("🚀 ~ file: app.js:10 ~ app.on ~ error:", err)
     });
     app.on('request', (ctx) => {
-        console.log("🚀 ~ file: app.js:13 ~ app.on ~ request:", ctx)
         // log receive request
     });
     app.on('response', (ctx) => {
         // ctx.starttime is set by framework
         const used = Date.now() - ctx.starttime;
-        console.log("🚀 ~ file: app.js:21 ~ app.on ~ response:", ctx)
+        console.log("🚀 ~ file: app.js:21 ~ app.on ~ response:", used)
         // log total cost
     });
 };
