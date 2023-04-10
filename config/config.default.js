619dd7ab42accf3a871fa6d771b58c4e72aab1f2
15239136251 <2532281393@qq.com>
2023-04-10 14:40:18 +0800

    将mysql配置切换到其他配置文件内

--- config/config.default.js
+++ config/config.default.js
@@ -22,29 +22,8 @@ module.exports = appInfo => {
     listen: {
       path: '',
       port: 8008,
-      hostname: '0.0.0.0'
+      hostname: '127.0.0.1'
     }
-  };
-
-  // add your mysql config
-  config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // IP
-      host: '1.116.143.16',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: 'quhaibin',
-      // 数据库名
-      database: 'all_system',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
   };
 
   // 添加jwt
